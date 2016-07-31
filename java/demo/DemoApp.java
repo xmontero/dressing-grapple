7ce90e0f4636c78f056177d06e90a1430c9f7c66
Xavi Montero <xmontero@dsitelecom.com>
2016-08-01 00:18:33 +0200

    Adds namespaces to the java classes. Namespace does not represent a reversed domain, but is chosen to be coherent with the PHP namespace.

--- java/demo/DemoApp.java
+++ java/demo/DemoApp.java
@@ -1,14 +1,26 @@
-class DemoApp
+package xavi_montero.dressing_grapple.java.demo;
+
+import xavi_montero.dressing_grapple.java.Calculator;
+
+public class DemoApp
 {
     public static void main( String[] args )
     {
+        System.out.println( "-----------------------------------------------" );
+        System.out.println( "xavi_montero.dressing_grapple.java.demo.DemoApp" );
+        System.out.println( "-----------------------------------------------" );
+        System.out.println();
+        System.out.println( "Explanation:" );
+        System.out.println( "    This java demo program calls the Calculator java class and executes the sum() method." );
+        System.out.println( "    The intention is check that the Calculator class is there, ready to be consumed from PHP." );
+        System.out.println( "    See the README.md for mor information." );
+        System.out.println();
+
+        int a = 3;
+        int b = 4;
         Calculator calculator = new Calculator();
 
-        System.out.println( "XaviMontero/DressingGrapple/Java/Demo" );
-        System.out.println( "=====================================" );
-        System.out.println( "This java demo program calls the Calculator java class and executes the sum() method." );
-        System.out.println( "The intention is check that the Calculator class is there, ready to be consumed from PHP." );
-        System.out.println( "See the README.md for mor information." );
-        System.out.println( "3 + 4 = " + calculator.sum( 3, 4 ) );
+        System.out.println( "Demo:" );
+        System.out.println( "    " + a + " + " + b + " = " + calculator.sum( a, b ) );
     }
 }
--- java/src/Calculator.java
+++ java/src/Calculator.java
@@ -1,4 +1,6 @@
-class Calculator
+package xavi_montero.dressing_grapple.java;
+
+public class Calculator
 {
     public int sum( int a, int b )
     {

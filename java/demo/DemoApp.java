788bf95502dadf4fb93dd23d71f5a9e90c1dfb68
Xavi Montero <xmontero@dsitelecom.com>
2016-07-31 17:50:20 +0200

    Adds the java initial project.
    Adds:
    - A minimal library in java (java/src/Calculator.java) to be consumed from PHP.
    - A demo app in java (java/demo/DemoApp.java) to be invoked from the command line to see if the calculator works properly.
    - The java sdk in the vagrant machine.
    - A tool to compile the java demo.
    - A tool to run the java demo.

---
+++ java/demo/DemoApp.java
@@ -0,0 +1,9 @@
+class DemoApp
+{
+    public static void main( String[] args )
+    {
+        Calculator calculator = new Calculator();
+
+        System.out.println( calculator.sum( 3, 4 ) );
+    }
+}
---
+++ java/src/Calculator.java
@@ -0,0 +1,8 @@
+class Calculator
+{
+    public int sum( int a, int b )
+    {
+        int result = a + b;
+        return result;
+    }
+}
